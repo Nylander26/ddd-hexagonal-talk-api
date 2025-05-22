6402cb8d6ac18149dc223d2d205b85ed1406a19a
Wolfgang Nylander <wolfnylander@gmail.com>
2025-05-22 16:24:03 +0200

    First commit

---
+++ src/test/java/com/vamoacomej/ddd_hexagonal/DddHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vamoacomej.ddd_hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
